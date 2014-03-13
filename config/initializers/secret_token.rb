2905550597b9b338e81033979ff0348cb01dec58
Dennis Anthony Acuna <dacuna30@academic.rrc.ca>
2014-03-13 09:30:11 -0500

    First commint
    Test for commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleStore::Application.config.secret_key_base = '69529f3900e82e8d848ac5929f6c8d1bcc3d2d7fbd64f042ca36b580c92575893289c993905cc68ce8064f12322352548363d8b4caf0e9cdc87f114db9fcce67'
